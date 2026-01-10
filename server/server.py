b06d91d23a9235c1d9db34eb42fdf324c770a3ea
Bekir Altindal <bekir.altindal@rwth-aachen.de>
2026-01-10 10:41:19 +0100

    refactor and fix demonstration

--- server/server.py
+++ server/server.py
@@ -1,18 +1,30 @@
 import socket
+import time
 
 HOST = 'localhost'
 PORT = 1453
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # SOCK_STREAM is basically TCP
-                                                           # DGRAM would be UDP
-server.bind((HOST, PORT))
-print(f"Server: Bound on {HOST}:{PORT}")
 
-server.listen()
-print(f"Server: Listening")
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.bind((HOST, PORT))
+    print(f"[SERVER] Bound on {HOST}:{PORT}")
 
-while True:
-    conn, address = server.accept()
-    message = conn.recv(1024) # Buffersize set to 1024 Bytes.
-                              # Receive up to 1024 Bytes.
-    conn.close()
-    print(f"Server: Message {message.decode('utf-8')} received from {address}")
+    server.listen()
+    print('[SERVER] Listening ...')
+
+    while True:
+        print('\n[SERVER] Waiting for a client ...')
+        start = time.time()
+        conn, address = server.accept()
+        print(f"[SERVER] A client connected after {time.time() - start:.2f} seconds")
+
+        start = time.time()
+        with conn:
+            message = conn.recv(1024) # Buffersize set to 1024 Bytes.
+            print(f"[SERVER] Client {address} sent {message.decode('utf-8')}. Processing Time {time.time() - start:.2f} seconds")
+
+            # Processing client's request ... assume it is a heavy I/O operation and client is waiting for a response
+            time.sleep(5)
+
+            message = 'Ve Aleykümselam'
+            conn.sendall(message.encode('utf-8'))
+            print(f"[SERVER] Replied {message} to client after {time.time() - start:.2f} seconds")
