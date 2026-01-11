4d87e3f12b3a2a85815bc9126b598e1fb375af7f
Bekir Altindal <bekir.altindal@rwth-aachen.de>
2026-01-11 10:01:14 +0100

    refactor: output

--- server/multiple_clients.py
+++ server/multiple_clients.py
@@ -13,12 +13,21 @@ def client(i):
     start = time.time()
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST, PORT))
-    message = 'Selamunaleyküm'
+    message = f"PING {i}"
     client.send(message.encode('utf-8'))
-    client.close()
 
     with print_lock:
         print(f"[CLIENT {i}] Sent {message} after {time.time() - start:.2f} seconds")
+
+    message = client.recv(1024)
+
+    with print_lock:
+        print(f"[CLIENT {i}] Received {message.decode('utf-8')} after {time.time() - start:.2f} seconds")
+
+    client.close()
+
+    with print_lock:
+        print(f"[CLIENT {i}] Closed connection.")
 
 threads = [threading.Thread(target=client, args=(i,)) for i in range(10)]
 
