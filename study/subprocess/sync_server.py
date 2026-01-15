68f9559034107ca714544a328fd1c97c94da6cf7
Bekir Altindal <bekir.altindal@rwth-aachen.de>
2026-01-15 22:24:34 +0100

    init: sync subprocess

---
+++ study/subprocess/sync_server.py
@@ -0,0 +1,21 @@
+import asyncio, logging, subprocess
+from asyncio import StreamReader, StreamWriter
+
+logging.basicConfig(level=logging.DEBUG, format="%(levelname)-7s %(name)-8s %(message)s")
+logger = logging.getLogger(__name__)
+
+async def callback(reader: StreamReader, writer: StreamWriter) -> None:
+    message = await reader.read(1024)
+    result = subprocess.run(
+        ['python', '../../example/echo.py', message.decode('utf-8')],
+        capture_output=True,
+        text=True
+    )
+    message = result.stdout
+    writer.write(message.encode('utf-8'))
+    writer.close()
+    await writer.wait_closed()
+
+with asyncio.Runner(debug=True) as runner:
+    server = runner.run(asyncio.start_server(callback, 'localhost', 1453))
+    runner.run(server.serve_forever())
