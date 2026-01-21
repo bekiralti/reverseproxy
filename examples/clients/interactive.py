05e0834695e6f89e55a5f2d71af81c352e89d64e
Bekir Altindal <bekir.altindal@rwth-aachen.de>
2026-01-21 12:03:47 +0100

    add: error handling for server/client disconnect

--- examples/clients/interactive.py
+++ examples/clients/interactive.py
@@ -3,5 +3,18 @@ import socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
     client.connect(('localhost', 1453))
     while True:
-        client.send(input('[CLIENT] Enter message: ').encode('utf-8'))
-        print(f"[CLIENT] Received: {client.recv(1024).decode('utf-8')}, from proc-id")
+        message = input('[CLIENT] Enter message: ')
+
+        if not message:
+            print('[CLIENT] Cannot send empty message, try again.')
+            continue
+
+        client.send(message.encode('utf-8'))
+        message = client.recv(1024)
+
+        if not message:
+            print('[CLIENT] Server disconnected')
+            break
+
+        print(f"[CLIENT] Received: {message.decode('utf-8')}")
+    client.close()
