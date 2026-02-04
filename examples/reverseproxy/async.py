88f4479d990fd782b6815b32319c6e3a59b0dd82
Bekir Altindal <bekir.altindal@rwth-aachen.de>
2026-02-04 06:37:00 +0100

    fix: pipes

--- examples/reverseproxy/async.py
+++ examples/reverseproxy/async.py
@@ -5,21 +5,24 @@ logging.basicConfig(level=logging.DEBUG, format="%(levelname)-7s %(name)-8s %(me
 logger = logging.getLogger(__name__)
 
 async def callback(reader: StreamReader, writer: StreamWriter) -> None:
+    process: asyncio.subprocess.Process = await asyncio.create_subprocess_exec(
+        'python',
+        '../servers/sockets/echo.py',
+        stdin=asyncio.subprocess.PIPE,
+        stdout=asyncio.subprocess.PIPE
+    )
+
     while True:
         message: bytes = await reader.read(1024)
-
         if not message:
-            print('TESTOSTERON')
             break
+        process.stdin.write(message)
+        await process.stdin.drain()
+        message = await process.stdout.read(1024)
+        writer.write(message)
+        await writer.drain()
 
-        process: asyncio.subprocess.Process = await asyncio.create_subprocess_exec(
-            'python',
-            '../echo.py',
-            message.decode('utf-8'),
-            stdout=asyncio.subprocess.PIPE
-        )
-        data: tuple[bytes, bytes] = await process.communicate() # data = (stdout_data, stderr_data)
-        writer.write(data[0])
+    process.kill()
     writer.close()
     await writer.wait_closed()
 
