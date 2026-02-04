19ed14f3dfe15aa64d50e3586f11023ed1606678
Bekir Altindal <bekir.altindal@rwth-aachen.de>
2026-02-04 09:52:24 +0100

    fix: uuid needs to be send as quickly as possible

--- examples/server.py
+++ examples/server.py
@@ -5,7 +5,6 @@ logger = logging.getLogger('server')
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect(('127.0.0.1', 3001))
-    logger.debug(s.getsockname())
     message = str(sys.argv[1]) + '\n'
     s.sendall(message.encode())
 
