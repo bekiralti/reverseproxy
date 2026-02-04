3298f363ea12159ae7ec635295a39c8d847a2456
Bekir Altindal <bekir.altindal@rwth-aachen.de>
2026-02-04 15:26:45 +0100

    fix: replace sync with async pattern

--- examples/client.py
+++ examples/client.py
@@ -1,12 +1,24 @@
-import logging, socket
+import asyncio, logging
 
 logging.basicConfig(level=logging.DEBUG, format="%(levelname)-7s %(name)-12s %(message)s")
 logger = logging.getLogger('client')
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(('127.0.0.1', 3000))
-    logger.debug(s.getsockname())
-
+async def read(reader):
     while True:
-        message = input('Send message: ') + '\n'
-        s.sendall(message.encode())
+        message = await reader.readline()
+        if not message:
+            break
+
+async def write(writer):
+    loop = asyncio.get_running_loop()
+    while True:
+        message = await loop.run_in_executor(None, input, 'Send: ')
+        writer.write((message + '\n').encode())
+        await writer.drain()
+
+async def main():
+    reader, writer = await asyncio.open_connection('127.0.0.1', 3000)
+    await asyncio.gather(read(reader), write(writer))
+    print("HMMMMMMMMMM!")
+
+asyncio.run(main(), debug=True)
