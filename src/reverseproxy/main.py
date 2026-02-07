782d6d4be980c4ae583f6a55667d585bf32ff1fe
Bekir Altindal <bekir.altindal@rwth-aachen.de>
2026-02-07 22:37:06 +0100

    refactoring: weitestgehend erledigt. Fehlt Umzug von kitty nach server.py

--- src/reverseproxy/main.py
+++ src/reverseproxy/main.py
@@ -1,4 +1,5 @@
 import asyncio, json, logging, subprocess, uuid
+import sys
 from dataclasses import dataclass
 from pathlib import Path
 
@@ -6,16 +7,8 @@ logging.basicConfig(level=logging.DEBUG, format="%(levelname)-7s %(name)-12s %(m
 logger = logging.getLogger('reverseproxy')
 
 connections = {}
-server_count = 0
-target_id = None
-
-async def get_rightmost_window():
-    """Finde IMMER das aktuell rechteste Fenster (kein right-Nachbar)"""
-    kitty_data = json.loads(subprocess.check_output(['kitty', '@', 'ls']))
-    active_tab = kitty_data[0]['tabs'][0]
-    right_window = next(w for w in active_tab['windows']
-                       if 'right' not in w.get('neighbors', {}))
-    return right_window['id']
+# server_count = 0
+# target_id = None
 
 @dataclass
 class Connection:
@@ -23,6 +16,14 @@ class Connection:
     client_writer: asyncio.StreamWriter
     server_reader: asyncio.StreamReader = None
     server_writer: asyncio.StreamWriter = None
+
+async def get_rightmost_window():
+    """Finde IMMER das aktuell rechteste Fenster (kein right-Nachbar)"""
+    kitty_data = json.loads(subprocess.check_output(['kitty', '@', 'ls']))
+    active_tab = kitty_data[0]['tabs'][0]
+    right_window = next(w for w in active_tab['windows']
+                        if 'right' not in w.get('neighbors', {}))
+    return right_window['id']
 
 async def forward(reader, writer):
     while True:
@@ -34,58 +35,12 @@ async def forward(reader, writer):
     writer.close()
     await writer.wait_closed()
 
-
 async def server(reader, writer):
-    message = await reader.readline()
-    message = uuid.UUID(message.decode().strip())
+    connection_id = uuid.UUID((await reader.readline()).decode().strip())
 
-    if message in connections:
-        connections[message].server_reader = reader
-        connections[message].server_writer = writer
-
-    try:
-        await asyncio.sleep(float('inf'))  # Warte ewig
-    except asyncio.CancelledError:
-        pass
-
-async def client(reader, writer):
-    global server_count, target_id
-
-    connection_id = uuid.uuid4()
-    connections[connection_id] = Connection(reader, writer)
-
-    # Kitty-Status abfragen
-    if target_id is None:
-        target_id = await get_rightmost_window()
-
-    # Berechne den absoluten Pfad zu server.py
-    script_dir = Path(__file__).parent.resolve()
-    server_script = script_dir / '../../examples/server.py'
-    server_script = server_script.resolve()  # Macht den Pfad absolut
-
-    if server_count == 0:
-        subprocess.Popen([
-            'kitty', '@', 'send-text', '--match', f"id:{target_id}",
-            f"python {str(server_script)} {str(connection_id)}\n"
-        ])
-    else:
-        # Fokus setzen
-        proc = await asyncio.create_subprocess_exec(
-            'kitty', '@', 'focus-window', '--match', f'id:{target_id}'
-        )
-        await proc.wait()
-
-        # Jetzt neuen server starten
-        subprocess.Popen([
-            'kitty', '@', 'launch', '--location=hsplit', '--match', f"id:{target_id}",
-            'python', str(server_script), str(connection_id)
-        ])
-    server_count += 1
-
-    for _ in range(50):
-        if connections[connection_id].server_writer:
-            break
-        await asyncio.sleep(0.1)
+    # Wichtig, damit die Zuordnung der Clients mit den Servern stimmt!
+    connections[connection_id].server_reader = reader
+    connections[connection_id].server_writer = writer
 
     await asyncio.gather(
         forward(
@@ -98,9 +53,55 @@ async def client(reader, writer):
         )
     )
 
+    # At this point, the connection has been closed
+    del connections[connection_id]
+
+async def client(reader, writer):
+    connection_id = uuid.uuid4()
+    if connection_id in connections:
+        writer.write('Fatal error: Generated UUID already exists. Please connect again.\n'.encode())
+        await writer.drain()
+        logger.fatal(f"Generated UUID:{connection_id} already exists")
+        writer.close()
+        await writer.wait_closed()
+        return
+    connections[connection_id] = Connection(client_reader=reader, client_writer=writer)
+
+    subprocess.Popen([sys.executable, '../../examples/server.py', str(connection_id)])
+
+    # global server_count, target_id
+    #
+    # # Kitty-Status abfragen
+    # if target_id is None:
+    #     target_id = await get_rightmost_window()
+    #
+    # # Berechne den absoluten Pfad zu server.py
+    # script_dir = Path(__file__).parent.resolve()
+    # server_script = script_dir / '../../examples/server.py'
+    # server_script = server_script.resolve()  # Macht den Pfad absolut
+    #
+    # if server_count == 0:
+    #     subprocess.Popen([
+    #         'kitty', '@', 'send-text', '--match', f"id:{target_id}",
+    #         f"python {str(server_script)} {str(connection_id)}\n"
+    #     ])
+    # else:
+    #     # Fokus setzen
+    #     proc = await asyncio.create_subprocess_exec(
+    #         'kitty', '@', 'focus-window', '--match', f'id:{target_id}'
+    #     )
+    #     await proc.wait()
+    #
+    #     # Jetzt neuen server starten
+    #     subprocess.Popen([
+    #         'kitty', '@', 'launch', '--location=hsplit', '--match', f"id:{target_id}",
+    #         'python', str(server_script), str(connection_id)
+    #     ])
+    # server_count += 1
+
 async def main():
-    clients = await asyncio.start_server(client, '127.0.0.1', 3000)
-    servers = await asyncio.start_server(server, '127.0.0.1', 3001)
+    clients = await asyncio.start_server(client, '127.0.0.1', 3000) # Handles the message forwardings
+    servers = await asyncio.start_server(server, '127.0.0.1', 3001) # Handles the server registrations
     async with clients, servers:
         await asyncio.gather(clients.serve_forever(), servers.serve_forever())
 
