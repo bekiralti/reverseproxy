bdcb4a9dbc4b1884715a8aba19ada15c40bd93a2
Bekir Altindal <bekir.altindal@rwth-aachen.de>
2026-02-07 16:24:18 +0100

    refactoring: main

--- src/reverseproxy/main.py
+++ src/reverseproxy/main.py
@@ -34,6 +34,20 @@ async def forward(reader, writer):
     writer.close()
     await writer.wait_closed()
 
+
+async def server(reader, writer):
+    message = await reader.readline()
+    message = uuid.UUID(message.decode().strip())
+
+    if message in connections:
+        connections[message].server_reader = reader
+        connections[message].server_writer = writer
+
+    try:
+        await asyncio.sleep(float('inf'))  # Warte ewig
+    except asyncio.CancelledError:
+        pass
+
 async def client(reader, writer):
     global server_count, target_id
 
@@ -83,19 +97,6 @@ async def client(reader, writer):
             connections[connection_id].client_writer
         )
     )
-
-async def server(reader, writer):
-    message = await reader.readline()
-    message = uuid.UUID(message.decode().strip())
-
-    if message in connections:
-        connections[message].server_reader = reader
-        connections[message].server_writer = writer
-
-    try:
-        await asyncio.sleep(float('inf'))  # Warte ewig
-    except asyncio.CancelledError:
-        pass
 
 async def main():
     clients = await asyncio.start_server(client, '127.0.0.1', 3000)
