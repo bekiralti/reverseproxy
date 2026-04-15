364658c2dad3471e0410c18a096d30a17f0a348a
Bekir Altindal <bekir.altindal@rwth-aachen.de>
2026-04-15 10:16:56 +0200

    fix: racing-condition fixed with asyncio.Future

--- src/main.py
+++ src/main.py
@@ -19,37 +19,19 @@ def graceful_shutdown(signum, frame):
     logger.info("Danke und bis bald! :)")
     sys.exit(0)
 
-async def reply_with_new_uuid(writer):
+def reply_with_new_uuid(browser_writer, browser_uuid):
     """Gebe dem Browser ein neues UUID-Cookie und sage ihm, dass er es gleich noch einmal versuchen soll"""
-    _uuid = str(uuid.uuid4()) # UUID4 ist einzigartig genug. Es reicht (vorerst) zur eindeutigen Identifikation
-    logger.debug(f"New UUID: {_uuid}")
-    writer.write((
+    browser_writer.write((
         "HTTP/1.1 200 OK\r\n"
-        f"Set-Cookie: uuid={_uuid}\r\n"
+        f"Set-Cookie: uuid={browser_uuid}\r\n"
         "Content-Type: text/html\r\n"
         "\r\n"
         "<html>"
         "  <head>"
-        "    <meta http-equiv='refresh' content='3'>"
+        "    <meta http-equiv='refresh' content='1'>"
         "  </head>"
         "</html>").encode()
     )
-    await writer.drain()
-    return _uuid
-
-async def run_new_docker_container(image):
-    """Starte einen neuen VILLASconf Docker-Container"""
-    loop = asyncio.get_running_loop()
-    logger.debug('Starte Docker-Container')
-    docker_container = await loop.run_in_executor(None, functools.partial(   # run_in_executor() only allows args. To allow kwargs we use functools.partial()
-        docker_client.containers.run,                                                # type: ignore
-        image,
-        detach=True,                                                                 # -d
-        ports={'1880/tcp': 0},                                                       # -p 0:1880 (0 lets the kernel choose a free port)
-        volumes={os.path.abspath('../docker/data'): {'bind': '/data', 'mode': 'rw'}} # -v ./docker/data:/data
-    ))                                                                               # type: ignore
-    logger.debug('Docker-Container gestartet')
-    return docker_container
 
 async def forward_message(reader, writer):
     while True:
@@ -78,44 +60,61 @@ async def client_connected_cb(browser_reader, browser_writer):
     browser_uuid = re.search(rb"Cookie:.*uuid=([a-f0-9-]+).*\r\n", browser_request)
     browser_uuid = browser_uuid.group(1).decode() if browser_uuid else None
 
-    # Edge-Case: Es können zwei verschiedene Browser, mit browser_uuid == None einen *falschen* Docker-Container zugewiesen bekommen
+    # Edge-Case: Zwei verschiedene Browser, die noch keine UUID haben, können einen *falschen* Docker-Container zugewiesen bekommen
     if browser_uuid is None:
         # Daher wird dieser Fall frühzeitig verlassen. Dem Browser wird eine UUID zugewiesen und aufgefordert es erneut zu versuchen
-        await reply_with_new_uuid(browser_writer)
+        browser_uuid = str(uuid.uuid4())
+        reply_with_new_uuid(browser_writer, browser_uuid)
+        await browser_writer.drain()
         browser_writer.close()
         return
 
-    # Existiert bereits ein Lock für diesen UUID?
-    if browser_uuid not in locks:
-        # Falls nein, dann erstelle eins
-        locks[browser_uuid] = asyncio.Lock()
-
     # Ist es eine gültige UUID?
-    old_uuid = browser_uuid
+    loop = asyncio.get_running_loop()
     if browser_uuid in sessions:
-        # Falls ja, dann existiert hierfür schon ein Docker-Container
-        docker_container = sessions[browser_uuid]
+        # Erwarten wir für diese UUID eine Docker-Container erstellung, oder wurde bereits ein Docker-Container erstellt?
+        future = sessions[browser_uuid]
+        if asyncio.isfuture(future):
+            # Wir erwarten noch die Docker-Container erstellung
+            docker_container = await future # Hier wird noch auf die Docker-Container erstellung gewartet
+        else:
+            # Docker-Container wurde bereits erstellt
+            docker_container = future # Hier ist future deer Docker-Container selbst. (Docker-Container sind nicht awaitable)
     else:
         # Falls nein, dann muss eine neue UUID und ein Docker-Container erstellt werden
-        # Jedoch kann hier eine Racing-Condition entstehen, wenn ein Browser zwei Requests *zu schnell* hintereinanderschickt
-        # Die Racing-Condition führt dazu, dass zwei oder sogar mehr Docker-Container für einen Browser erstellt werden
-        # Dadurch können mehrere Docker-Container entstehen, die verwaisen
-        # Zur Auflösung der Racing-Condition wird hier für die Cookie-UUID ein Lock erstellt
-        # Innerhalb des gelockten Bereichs wird geprüft, ob nicht schon eine vorausgehende TCP-Verbindung vom selben Browser bereits einen Docker-Container erstellt hat
-        # Wenn ein Docker-Container für diesen Browser bereits erstellt worden ist, dann gilt: old_uuid != browser_uuid
-        async with locks[browser_uuid]:
-            if old_uuid == browser_uuid:
-                # Es wurde für diesen Browser noch kein Docker-Container erstellt
-                browser_uuid = await reply_with_new_uuid(browser_writer)
-                docker_container = await run_new_docker_container('my-villas-image')
-                sessions[browser_uuid] = docker_container
-            else:
-                # Es wurde für diesen Browser bereits ein Docker-Container erstellt
-                docker_container = sessions[browser_uuid]
-            locks.pop(old_uuid)
+        browser_uuid = str(uuid.uuid4())
+
+        # Future: Merke dir, dass für diese UUID (hoffentlich) ein Docker-Container erstellt werden wird
+        future = loop.create_future()
+        sessions[browser_uuid] = future
+        reply_with_new_uuid(browser_writer, browser_uuid)
+        await browser_writer.drain()
+        browser_writer.close()
+
+        # Erinnerung: An `await` Stellen ist immer Concurrency! D.h. an dieser Stelle ist der Programmfluss NICHT mehr synchron!
+        # Bis der Docker-Container (in den folgenden Zeilen) erstellt wird, kann bspw. vom selben Browser ein neuer Request kommen.
+        # Dieser neue Request kann dann im if-Fall landen, da wir ja jetzt eine *gültige* UUID haben.
+        logger.debug('Starte Docker-Container')
+        try:
+            docker_container = await loop.run_in_executor(None, functools.partial(    # run_in_executor() only allows args. To allow kwargs we use functools.partial()
+                docker_client.containers.run,                                                 # type: ignore
+                'my-villas-image',
+                detach=True,                                                                  # -d
+                ports={'1880/tcp': 0},                                                        # -p 0:1880 (0 lets the kernel choose a free port)
+                volumes={os.path.abspath('../docker/data'): {'bind': '/data', 'mode': 'rw'}}  # -v ./docker/data:/data
+            ))                                                                                # type: ignore
+            sessions[browser_uuid] = docker_container
+            future.set_result(docker_container)  # `await`s die auf dieses Ergebnis warten (await future) werden hiermit *entblockt*
+        except Exception as e:
+            # Nur für den Fall, dass die Docker-Container erstellung fehlschlägt
+            del sessions[browser_uuid]
+            future.set_exception(e) # Wenn future nicht gesetzt wird, *wartet* await future ewig
+            logger.error(f"Fehler bei Docker-Container erstellung: {e}")
+            raise
+        logger.debug('Docker-Container gestartet')
+        return
 
     # Ist der Docker-Container bereit?
-    loop = asyncio.get_running_loop()
     await loop.run_in_executor(None, docker_container.reload) # type: ignore
     logger.debug(docker_container.health)
     if docker_container.health != 'healthy':
@@ -126,7 +125,7 @@ async def client_connected_cb(browser_reader, browser_writer):
             "\r\n"
             "<html>"
             "  <head>"
-            "    <meta http-equiv='refresh' content='3'>"
+            "    <meta http-equiv='refresh' content='1'>"
             "  </head>"
             "</html>").encode()
         )
