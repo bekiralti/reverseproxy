eb1225cf93d70b6a1b600fc4f4a4105da0c21638
Bekir Altindal <bekir.altindal@rwth-aachen.de>
2026-05-05 15:37:58 +0200

    error-handling, logging and path attribute for Session
    Added the path attribute to have the path defined at *one place* and not heard-coded at various lines throughout the code.

--- src/main.py
+++ src/main.py
@@ -18,29 +18,31 @@ sessions = {}  # Look-up-table
 @dataclass(slots=True)
 class Session:
     docker_container: Container
+    path: str
     last_seen: float
 
 def shutdown_gracefully(signum, frame):
-    logger.debug(f"Signal: {signum}, Frame: {frame}")
-    logger.debug("Reverse-proxy shuts down")
+    logger.info(f"Signal: {signum}, Frame: {frame}")
+    logger.info("Reverse-proxy shuts down")
     for uuid4, session in sessions.items():
-        logger.debug(f"Stop and remove Docker-Container {session.docker_container} and delete its associated data: ./docker/data-{uuid4}")
+        logger.info(f"Stop and remove Docker-Container {session.docker_container} and delete its associated data at path: {session.path}")
         if session.docker_container:
             session.docker_container.stop()
             session.docker_container.remove()
-        shutil.rmtree(os.path.abspath(f'../docker/data-{uuid4}'))
+        shutil.rmtree(session.path)
     logger.info("Thank you and see you soon! :)")
     sys.exit(0)
 
-# Alternativ: Inaktive Container nicht über dieses Polling, sondern über WebSocket-Close-Codes erkennen. Oder beides.
 async def poll_sessions():
     while True:
         for uuid4, session in list(sessions.items()):
             elapsed_time = time.time() - session.last_seen
+
+            # A *too tight* window leads to the deletion of the container before it can be even used
             if elapsed_time > 60:
                 await asyncio.to_thread(session.docker_container.stop)    # IO-Bound
                 await asyncio.to_thread(session.docker_container.remove)  # IO-Bound
-                shutil.rmtree(os.path.abspath(f'../docker/data-{uuid4}'))
+                shutil.rmtree(session.path)                               # TODO: Make this asyncio
                 del sessions[uuid4]
         await asyncio.sleep(60)  # Polling every 1 minute (60 seconds). Node-RED heartbeat happens every ~15 seconds
 
@@ -69,9 +71,9 @@ async def get_or_create_docker_container(uuid4: str) -> Container | None:
     Returns None if the Docker-Container is being created.
     You have to *retry* with the same `uuid4` to get the created Docker-Container.
 
-    TODO: A malicious client can spam randomly generated uuid4 cookies and thus create multiple Docker-Containers.
+    TODO: A malicious client can spam randomly generated UUID4 cookies and thus create multiple Docker-Containers.
           One solution is to remember the IP address and limit the amount of Docker-Containers this IP address can spawn.
-          E.g. 1 Docker-Container every 60 seconds.
+          E.g. 1 Docker-Container every 60 seconds. Other solutions are also possible.
     """
 
     if uuid4 in sessions:
@@ -81,19 +83,18 @@ async def get_or_create_docker_container(uuid4: str) -> Container | None:
         sessions[uuid4] = None  # Now, `uuid4 in sessions` evaluates to `True`
 
         # Each Browser has to get its own data folder, otherwise they will conflict each other!
-        os.makedirs(os.path.abspath(f"../docker/data-{uuid4}"))
-
-        # Create a coroutine out of the synchronous IO-Bound method call run() and schedule it as a task
+        path = os.path.abspath(f"../data/{uuid4}")
+        os.makedirs(path)  # TODO: Make this also async, becaus it is IO-bound
         docker_container = await asyncio.to_thread(
             docker_client.containers.run,
             'my-villas-image',
-            detach=cast(Literal[True], True),                                                      # -d, `cast(Literal[True], True)` instead of simply `True` just to calm down PyCharm
-            ports={'1880/tcp': 0},                                                                 # -p 0:1880 (0 lets the kernel choose a free port)
-            volumes={os.path.abspath(f"../docker/data-{uuid4}"): {'bind': '/data', 'mode': 'rw'}}  # -v ./docker/data:/data
+            detach=cast(Literal[True], True),                                               # -d, `cast(Literal[True], True)` instead of simply `True` just to calm down PyCharm
+            ports={'1880/tcp': 0},                                                          # -p 0:1880 (0 lets the kernel choose a free port)
+            volumes={path: {'bind': '/data', 'mode': 'rw'}}  # -v ./docker/data:/data
         )
 
         # Update the above Dummy with the actual Session object
-        sessions[uuid4] = Session(docker_container=docker_container, last_seen=time.time())
+        sessions[uuid4] = Session(docker_container=docker_container, path=path, last_seen=time.time())
 
         return docker_container
 
@@ -114,51 +115,52 @@ def parse_uuid4(text):
     return uuid4.group(1).decode() if uuid4 else None
 
 # Initially added these type-hints to calm down PyCharm. Meanwhile, code changed. Not sure if they are still necessary.
-async def read_http_header_and_body(
-        reader: StreamReader,
-) -> tuple[bytes, bytes]:
+async def read_http_header_and_body(reader: StreamReader) -> tuple[bytes, bytes]:
     """Returns a tuple: `(http_header, http_body)`.
 
     Returns `(http_header, b'')` if no `Content-Length` is specified.
 
     Returns the tuple `(b'', b'')` if no message is received after `timeout` seconds.
 
-    TODO: Eventually tighten the timeout value. Maybe 1 seconds instead of 10 seconds.
+    TODO: Eventually tighten the timeout value. Maybe 1 seconds instead of 10 seconds?
     """
 
     # Timeout for when someone connects and doesn't send an *HTTP-Request* or just blocks the line
-    try:
-        # HTTP-Header and HTTP-Body are separated by a blank line
-        http_header = await asyncio.wait_for(reader.readuntil(b'\r\n\r\n'), 10)
-    except TimeoutError:
-        return b'', b''
+    http_header = await asyncio.wait_for(reader.readuntil(b'\r\n\r\n'), 10)
 
-    # Based on Content-Length read the HTTP-Body
+    # TODO: Other options such as Chunked exist too beside Content-Length. One option amongst many: use aiohttp?
     content_length = re.search(
         rb"Content-Length: (\d+)",
         http_header,
         re.IGNORECASE  # HTTP-Headers are case-insensitive (see RFC 9110)
     )
     content_length = int(content_length.group(1)) if content_length else 0
+
     http_body = await reader.readexactly(content_length)
 
     return http_header, http_body
 
 async def reverseproxy_handler(client_reader, client_writer):
-    # Read UUID-Cookie
-    client_http_header, client_http_body = await read_http_header_and_body(client_reader)
-    logger.debug(f"HTTP-Request: {client_http_header}")
-    logger.debug(f"HTTP-Request: {client_http_body}")
-    if not client_http_header and not client_http_body:
-        logger.debug("The Client took too long to send an HTTP-Request!")
+    # Immediately try to read the HTTP-Request and UUID-Cookie
+    try:
+        client_http_header, client_http_body = await read_http_header_and_body(client_reader)
+    except IncompleteReadError:
+        logger.info("Client closed the connection immediately or just couldn't fully send a proper HTTP-Request")
         client_writer.close()
         await client_writer.wait_closed()
         return
+    except TimeoutError:
+        logger.info("The Client took too long to send an HTTP-Request!")
+        client_writer.close()
+        await client_writer.wait_closed()
+        return
+    logger.info(f"HTTP-Request: {client_http_header}")
+    logger.info(f"HTTP-Request: {client_http_body}")
     session_uuid4 = parse_uuid4(client_http_header)
 
     # Prevent multiple docker-container creations by someone who for example simply spams: curl my-proxy.net
     if session_uuid4 is None:
-        session_uuid4 = uuid.uuid4()
+        session_uuid4 = uuid.uuid4()  # Assumption: UUID4 is unique enough that collisions won't happen
         try_again(client_writer, session_uuid4)
         await client_writer.drain()
         client_writer.close()
@@ -188,13 +190,13 @@ async def reverseproxy_handler(client_reader, client_writer):
     try:
         docker_http_header, docker_http_body = await read_http_header_and_body(docker_reader)
     except ConnectionResetError, IncompleteReadError:
-        logger.debug("The Docker-Container is not ready yet. Tell the Client to try again.")
+        logger.info("The Docker-Container is not ready yet. Telling the Client to try again.")
         try_again(client_writer)
         await client_writer.drain()
         client_writer.close()
         await client_writer.wait_closed()
         return
-    logger.debug(f"HTTP-Response: {docker_http_header}")
+    logger.info(f"HTTP-Response: {docker_http_header}")
     logger.debug(f"HTTP-Response: {docker_http_body}")
 
     # HTTP-Response des Docker-Containers an den Browser weiterleiten
@@ -207,7 +209,7 @@ async def reverseproxy_handler(client_reader, client_writer):
             message = await reader.read(4096)                # 4 KiB (angelehnt an Pages) ist hier willkürlich gewählt
             logger.debug(f"Forward message: {message}")
             if not message:
-                break                                        # An *empty* message means disconnect
+                break                                        # *empty* `message` bedeutet die Verbindung wurde beendet
             sessions[session_uuid4].last_seen = time.time()  # *Heartbeat*
             writer.write(message)
             await writer.drain()
@@ -219,34 +221,17 @@ async def reverseproxy_handler(client_reader, client_writer):
         forward_message(docker_reader, client_writer)
     )
 
-async def webui_handler(reader, writer):
-    # Read HTTP-Request and HTTP-Body
-    http_header, http_body = await read_http_header_and_body(reader)
-
-    # Send JS with SSE
-    writer.write((
-        'HTTP/1.1 200 OK\r\n'
-        'Content-Type: application/javascript\r\n'
-        '\r\n'
-        '<html>'
-        '  <head>'
-        '    <script src="/webui.js"></script>'
-        '  </head>'
-        '</html>'
-    ))
-
-# The `port` parameter is soon going to become constant 443 for HTTPS
+# TODO: Use port 443 for HTTPS instead of a *random* one like 3000. HTTPS needs TLS and whatnot.
 async def main():
     signal.signal(signal.SIGINT, shutdown_gracefully)
     reverseproxy = await asyncio.start_server(reverseproxy_handler, '0.0.0.0', 3000)
-    webui = await asyncio.start_server(webui_handler, 'localhost', 4000)
     async with reverseproxy:
-        await asyncio.gather(reverseproxy.serve_forever(), poll_sessions(), webui.serve_forever())
+        await asyncio.gather(reverseproxy.serve_forever(), poll_sessions())
 
 if __name__ == '__main__':
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         format="%(asctime)s.%(msecs)03d %(levelname)-7s %(name)-12s %(message)s",
         datefmt="%H:%M:%S"
     )
-    asyncio.run(main(), debug=None)
+    asyncio.run(main(), debug=True)
