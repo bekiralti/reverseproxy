3fea33033df5451dc6431a1a3605a5085abfb4b8
Bekir Altindal <bekir.altindal@rwth-aachen.de>
2026-05-07 15:58:34 +0200

    Sketched out a plan

--- src/webui.js
+++ src/webui.js
@@ -1,8 +1,18 @@
+// Globale Variablen
 const es = new EventSource("/events");  // This basically initiates the SSE
+let current_ids = Set();
 
 // The function assigned to onmessage gets called by the Client's Browser whenever an SSE arrives
-es.onmessage = (event) => {
-    console.log(event.data)
-};
+es.onmessage = (e) => {
+    const data = JSON.parse(e.data);                     // Making an array out the e.data string
+    const ids = new Set(data.map(d => d.connection_id))  // We are extracting the id numbers and saving them as a Set
 
-console.log("ENDE");
+    // Hinzufügen
+    for id in ids:
+        if !current_ids.has(id):
+            // Hinzufügen
+
+    // Entfernen
+    for id in ids
+        // Ehm...
+};
